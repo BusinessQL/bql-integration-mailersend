1afcd2c07b8aa91211bdbf20f3eeb4e8711e7023
Marcel Thomas <marcelthomas5@gmail.com>
2021-11-08 12:22:58 +0000

    Added response headers

--- function/handler.js
+++ function/handler.js
@@ -3,10 +3,13 @@
 module.exports = async (event, context) => {
   const result = {
     name: 'mailersend',
-    version: '0.0.1-test',
+    version: '0.0.2-test',
     event,
     context,
   };
 
-  return context.status(200).succeed(result);
+  return context
+    .status(200)
+    .headers({ 'content-type': 'application/json' })
+    .succeed(result);
 };
