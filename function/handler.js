573b2c2f5d345812143b36300465be8fff1923fa
Marcel Thomas <marcelthomas5@gmail.com>
2021-11-08 11:24:03 +0000

    Updated details

--- function/handler.js
+++ function/handler.js
@@ -3,8 +3,8 @@
 module.exports = async (event, context) => {
   const result = {
     name: 'mailersend',
-    body: JSON.stringify(event.body),
-    'content-type': event.headers['content-type'],
+    version: '0.0.1',
+    'content-type': 'application/json',
   };
 
   return context.status(200).succeed(result);
